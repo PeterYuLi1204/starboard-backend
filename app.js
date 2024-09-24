7cc32679933721e8fa05857cd17be0d2d0e7fc09
Peter <peteryuli1204@gmail.com>
2024-09-23 23:20:02 -0700

    created a GET and POST endpoint for tasks

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 import express from "express";
+import tasks from "./routes/tasks.js"
 
 const app = express();
 const router = express.Router();
 const port = 3000;
 
+app.use('/tasks', tasks);
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World!');
 });
   
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`);
 });
