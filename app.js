cbe5050dddd1478ce967a4c42ce6699a4737e9a4
Peter <peteryuli1204@gmail.com>
2024-09-24 23:48:58 -0700

    changed cors options to allow all for development temporarily

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 import tasks from "./routes/tasks.js";
 
 const app = express();
 const router = express.Router();
 const port = process.env.PORT || 3000;
+
+// allows all origins (DISABLE LATER)
+app.use(cors());
 
 app.use(express.json());
 
